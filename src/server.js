a35489136166462733938da3f494dcf3e74aff07
mateustech <mateustech686@gmail.com>
2020-01-08 10:54:02 -0300

    Socket Concluido

--- src/server.js
+++ src/server.js
@@ -1,3 +1,38 @@
-import app from './app'
+import express from 'express'
+import routes from './routes'
+import mongoose from 'mongoose'
+import path from 'path'
+import cors from 'cors'
+const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-app.listen(3333)
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box)
+  })
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+app.use(routes)
+
+mongoose.connect('mongodb+srv://admin:admin@server-vg6ba.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongooDB Conectado!')
+}).catch(err => {
+  console.error('Deu Ruim na Conexao', err)
+})
+
+app.use((req, res, next) => {
+  req.io = io
+
+  return next()
+})
+
+server.listen(3333)
